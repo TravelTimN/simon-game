8e099610c01b8d648761752265bfaa0d30b54453
Tim Nelson <traveltimn@gmail.com>
2019-11-10 21:06:52 +0000

    Initial start of JS functions

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    let order = [];
+    let playerOrder = [];
+    let flash;
+    let turn;
+    let good;
+    let comptTurn;
+    let intervalId;
+    let strict = false;
+    let noise = true;
+    let on = false;
+    let win;
+
+    const turnCounter = document.querySelector("#turn");
+    const red = document.querySelector("#btn-red");
+    const yellow = document.querySelector("#btn-yellow");
+    const green = document.querySelector("#btn-green");
+    const blue = document.querySelector("#btn-blue");
+    const strictButton = document.querySelector("#strict");
+    const powerButton = document.querySelector("#power");
+    const startButton = document.querySelector("#start");
+
     
 
 });
