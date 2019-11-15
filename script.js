4f003490bac1d34ccee44ff0cb36cd02496f26a2
Tim Nelson <traveltimn@gmail.com>
2019-11-15 13:32:56 +0000

    Update buttons on click with animation and audio

--- script.js
+++ script.js
@@ -13,14 +13,52 @@ document.addEventListener("DOMContentLoaded", function() {
     let win;
 
     const turnCounter = document.querySelector("#turn");
-    const red = document.querySelector("#btn-red");
     const yellow = document.querySelector("#btn-yellow");
-    const green = document.querySelector("#btn-green");
     const blue = document.querySelector("#btn-blue");
+    const green = document.querySelector("#btn-green");
+    const red = document.querySelector("#btn-red");
+    const yellowAudio = document.querySelector("#yellowAudio");
+    const blueAudio = document.querySelector("#blueAudio");
+    const greenAudio = document.querySelector("#greenAudio");
+    const redAudio = document.querySelector("#redAudio");
     const strictButton = document.querySelector("#strict");
     const powerButton = document.querySelector("#power");
     const startButton = document.querySelector("#start");
 
-    
+    function yellowClick() {
+        yellow.classList.add("active");
+        yellowAudio.play();
+        setTimeout(function() {
+            yellow.classList.remove("active");
+        }, 100);
+    }
+    yellow.addEventListener("click", yellowClick);
+
+    function blueClick() {
+        blue.classList.add("active");
+        blueAudio.play();
+        setTimeout(function() {
+            blue.classList.remove("active");
+        }, 100);
+    }
+    blue.addEventListener("click", blueClick);
+
+    function greenClick() {
+        green.classList.add("active");
+        greenAudio.play();
+        setTimeout(function() {
+            green.classList.remove("active");
+        }, 100);
+    }
+    green.addEventListener("click", greenClick);
+
+    function redClick() {
+        red.classList.add("active");
+        redAudio.play();
+        setTimeout(function() {
+            red.classList.remove("active");
+        }, 100);
+    }
+    red.addEventListener("click", redClick);
 
 });
