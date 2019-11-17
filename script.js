028d5346bb29de8586e6ab546fddd3ceddad554a
Tim Nelson <traveltimn@gmail.com>
2019-11-17 20:08:39 +0000

    Create original audio-Hz sounds and lose sound

--- script.js
+++ script.js
@@ -24,24 +24,35 @@ document.addEventListener("DOMContentLoaded", function () {
     function pushButton(e) {
         let btnKey = "";
         if (e instanceof KeyboardEvent) {
+            // keyboard event (typing 'R', 'G', 'B', 'Y')
             btnKey = e.keyCode;
         } else if (e instanceof MouseEvent) {
+            // click event per button
             btnKey = this.dataset.key;
         }
+        // get dataset.key from audio element
         const audio = document.querySelector(`audio[data-key="${btnKey}"]`);
+        // get dataset.key from div.btn element
         const btn = document.querySelector(`.btn[data-key="${btnKey}"]`);
         if (!audio) return;
+        // reset audio to 0
         audio.currentTime = 0;
+        // pause any existing audio elements
+        var sounds = document.getElementsByTagName("audio");
+        for(i=0; i<sounds.length; i++) sounds[i].pause();
+        // play current audio element
         audio.play();
+        // add 'active' class
         btn.classList.add("active");
+        // timeout to remove 'active' class
         setTimeout(function () {
             btn.classList.remove("active");
         }, 100);
     }
-    window.addEventListener("keydown", pushButton);
-    green.addEventListener("click", pushButton);
-    red.addEventListener("click", pushButton);
-    blue.addEventListener("click", pushButton);
-    yellow.addEventListener("click", pushButton);
+    window.addEventListener("keydown", pushButton); // keyCode
+    green.addEventListener("click", pushButton); // click green
+    red.addEventListener("click", pushButton); // click red
+    blue.addEventListener("click", pushButton); // click blue
+    yellow.addEventListener("click", pushButton); // click yellow
 
 });
