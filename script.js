86276efcaf201c59d98bd2d2fc98a91fa9fd3ec8
Tim Nelson <traveltimn@gmail.com>
2019-11-23 23:03:41 +0000

    Fix some audio errors with win/lose

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let noise = true; // play noise
     let on = false; // is game on|off?
     let win; // has won?
+    let orderColors = [];
 
     const turnCounter = document.querySelector("#turn");
     const green = document.querySelector("#btn-green");
@@ -22,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const powerButton = document.querySelector("#power");
     const startButton = document.querySelector("#checkbox-start");
     const startButtonLabel = document.querySelector(".start label");
-    const powerOnAudio = document.querySelector("#powerOnAudio");
+    const winAudio = document.querySelector("#winAudio");
     const loseAudio = document.querySelector("#loseAudio");
 
     startButton.setAttribute("disabled", "disabled");
@@ -50,6 +51,7 @@ document.addEventListener("DOMContentLoaded", function () {
             turnCounter.innerHTML = "";
             clearColor();
             clearInterval(intervalId);
+            stopSounds();
         }
     });
 
@@ -65,6 +67,7 @@ document.addEventListener("DOMContentLoaded", function () {
     function play() {
         win = false;
         order = [];
+        orderColors = [];
         playerOrder = [];
         flash = 0;
         intervalId = 0;
@@ -73,10 +76,42 @@ document.addEventListener("DOMContentLoaded", function () {
         good = true;
         for (let i = 0; i < 31; i++) {
             // push a random whole number (1-4) onto the 'order' array
-            order.push(Math.floor(Math.random() * 4) + 1);
+            let randNum = Math.floor(Math.random() * 4) + 1;
+            order.push(randNum);
+            // order.push(Math.floor(Math.random() * 4) + 1);
+            if (randNum === 1) {
+                orderColors.push("green");
+            } else if (randNum === 2) {
+                orderColors.push("red");
+            } else if (randNum === 3) {
+                orderColors.push("yellow");
+            } else if (randNum === 4) {
+                orderColors.push("blue");
+            }
         }
+        console.log(orderColors);
         compTurn = true; // computer starts
-        intervalId= setInterval(gameTurn, 800) // pause 0.8s between each round
+        intervalId = setInterval(gameTurn, 800) // pause 0.8s between each round
+    }
+
+    let audioLength = function (audio) {
+        // shorten the audio time the further you play
+        if (turn <= 5) {
+            setTimeout(() => {
+                audio.pause();
+                audio.currentTime = 0;
+            }, 420);
+        } else if (turn >= 6 && turn <= 13) {
+            setTimeout(() => {
+                audio.pause();
+                audio.currentTime = 0;
+            }, 320);
+        } else {
+            setTimeout(() => {
+                audio.pause();
+                audio.currentTime = 0;
+            }, 220);
+        }
     }
 
     function gameTurn() {
@@ -87,6 +122,13 @@ document.addEventListener("DOMContentLoaded", function () {
         yellow.style.cursor = "default";
 
         if (flash == turn) {
+
+            window.addEventListener("keydown", pushButton);
+            green.addEventListener("click", pushButton);
+            red.addEventListener("click", pushButton);
+            blue.addEventListener("click", pushButton);
+            yellow.addEventListener("click", pushButton);
+
             green.style.cursor = "pointer";
             red.style.cursor = "pointer";
             blue.style.cursor = "pointer";
@@ -100,57 +142,55 @@ document.addEventListener("DOMContentLoaded", function () {
         if (compTurn) {
             clearColor();
             setTimeout(() => {
-                if (order[flash] == 1) one();
-                if (order[flash] == 2) two();
-                if (order[flash] == 3) three();
-                if (order[flash] == 4) four();
+                switch (order[flash]) {
+                    case 1:
+                        if (noise) {
+                            let audio = document.querySelector("#greenAudio");
+                            stopSounds();
+                            audio.currentTime = 0;
+                            audio.play();
+                            audioLength(audio);
+                        }
+                        noise = true;
+                        green.classList.add("active");
+                        break;
+                    case 2:
+                        if (noise) {
+                            let audio = document.querySelector("#redAudio");
+                            stopSounds();
+                            audio.currentTime = 0;
+                            audio.play();
+                            audioLength(audio);
+                        }
+                        noise = true;
+                        red.classList.add("active");
+                        break;
+                    case 3:
+                        if (noise) {
+                            let audio = document.querySelector("#yellowAudio");
+                            stopSounds();
+                            audio.currentTime = 0;
+                            audio.play();
+                            audioLength(audio);
+                        }
+                        noise = true;
+                        yellow.classList.add("active");
+                        break;
+                    case 4:
+                        if (noise) {
+                            let audio = document.querySelector("#blueAudio");
+                            stopSounds();
+                            audio.currentTime = 0;
+                            audio.play();
+                            audioLength(audio);
+                        }
+                        noise = true;
+                        blue.classList.add("active");
+                        break;
+                }
                 flash++;
             }, 200);
         }
-    }
-
-    function one() {
-        if (noise) {
-            let audio = document.querySelector("#greenAudio");
-            stopSounds();
-            audio.currentTime = 0;
-            audio.play();
-        }
-        noise = true;
-        green.classList.add("active");
-    }
-
-    function two() {
-        if (noise) {
-            let audio = document.querySelector("#redAudio");
-            stopSounds();
-            audio.currentTime = 0;
-            audio.play();
-        }
-        noise = true;
-        red.classList.add("active");
-    }
-
-    function three() {
-        if (noise) {
-            let audio = document.querySelector("#yellowAudio");
-            stopSounds();
-            audio.currentTime = 0;
-            audio.play();
-        }
-        noise = true;
-        yellow.classList.add("active");
-    }
-
-    function four() {
-        if (noise) {
-            let audio = document.querySelector("#blueAudio");
-            stopSounds();
-            audio.currentTime = 0;
-            audio.play();
-        }
-        noise = true;
-        blue.classList.add("active");
     }
 
     function clearColor() {
@@ -167,58 +207,6 @@ document.addEventListener("DOMContentLoaded", function () {
         blue.classList.add("active");
     }
 
-    green.addEventListener("click", () => {
-        if (on) {
-            playerOrder.push(1);
-            check();
-            one();
-            if (!win) {
-                setTimeout(() => {
-                    clearColor();
-                }, 300);
-            }
-        }
-    });
-
-    red.addEventListener("click", () => {
-        if (on) {
-            playerOrder.push(2);
-            check();
-            two();
-            if (!win) {
-                setTimeout(() => {
-                    clearColor();
-                }, 300);
-            }
-        }
-    });
-
-    yellow.addEventListener("click", () => {
-        if (on) {
-            playerOrder.push(3);
-            check();
-            three();
-            if (!win) {
-                setTimeout(() => {
-                    clearColor();
-                }, 300);
-            }
-        }
-    });
-
-    blue.addEventListener("click", () => {
-        if (on) {
-            playerOrder.push(4);
-            check();
-            four();
-            if (!win) {
-                setTimeout(() => {
-                    clearColor();
-                }, 300);
-            }
-        }
-    });
-
     function check() {
         if (playerOrder[playerOrder.length - 1] !== order[playerOrder.length - 1]) {
             good = false;
@@ -229,6 +217,13 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         if (good == false) {
+
+            window.removeEventListener("keydown", pushButton);
+            green.removeEventListener("click", pushButton);
+            red.removeEventListener("click", pushButton);
+            blue.removeEventListener("click", pushButton);
+            yellow.removeEventListener("click", pushButton);
+
             stopSounds();
             loseAudio.currentTime = 0;
             loseAudio.play(); // play lose sound
@@ -243,7 +238,10 @@ document.addEventListener("DOMContentLoaded", function () {
                 clearColor();
 
                 if (strict) {
-                    play();
+                    setTimeout(() => {
+                        order
+                        play();
+                    }, 800);
                 } else {
                     compTurn = true;
                     flash = 0;
@@ -266,14 +264,22 @@ document.addEventListener("DOMContentLoaded", function () {
             } else {
                 turnCounter.innerHTML = turn;
             }
-            intervalId = setInterval(gameTurn, 800);
+
+            // speed up the game the further you get
+            if (turn <= 5) {
+                intervalId = setInterval(gameTurn, 500);
+            } else if (turn >= 6 && turn <= 13) {
+                intervalId = setInterval(gameTurn, 400);
+            } else {
+                intervalId = setInterval(gameTurn, 300);
+            }
         }
     }
 
     function winGame() {
         stopSounds();
-        powerOnAudio.currentTime = 0;
-        powerOnAudio.play(); // play power-on music
+        winAudio.currentTime = 0;
+        winAudio.play(); // play power-on music
         flashColor();
         turnCounter.innerHTML = "☺";
         on = false;
@@ -282,83 +288,54 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // pause any existing audio elements
     function stopSounds() {
-        var sounds = document.getElementsByTagName("audio");
-        for(i=0; i<sounds.length; i++) sounds[i].pause();
+        var sounds = document.getElementsByTagName("audio:not(#loseAudio):not(#winAudio)");
+        for (i = 0; i < sounds.length; i++) sounds[i].pause();
     }
 
-    // power button (on|off)
-    // powerButton.addEventListener("click", () => {
-    //     on = !on;
-    //     if (on) { // power is on
-    //         stopSounds();
-    //         powerOnAudio.currentTime = 0;
-    //         powerOnAudio.play(); // play power-on music
-    //         green.style.cursor = "pointer";
-    //         red.style.cursor = "pointer";
-    //         blue.style.cursor = "pointer";
-    //         yellow.style.cursor = "pointer";
-    //         window.addEventListener("keydown", pushButton); // enable keyCode press
-    //         green.addEventListener("click", pushButton); // enable click green
-    //         red.addEventListener("click", pushButton); // enable click red
-    //         blue.addEventListener("click", pushButton); // enable click blue
-    //         yellow.addEventListener("click", pushButton); // enable click yellow
-    //     } else { // power is off
-    //         stopSounds();
-    //         loseAudio.currentTime = 0;
-    //         loseAudio.play(); // play 42Hz 'lose' audio
-    //         green.style.cursor = "default";
-    //         red.style.cursor = "default";
-    //         blue.style.cursor = "default";
-    //         yellow.style.cursor = "default";
-    //         window.removeEventListener("keydown", pushButton); // disable keyCode press
-    //         green.removeEventListener("click", pushButton); // disable click green
-    //         red.removeEventListener("click", pushButton); // disable click red
-    //         blue.removeEventListener("click", pushButton); // disable click blue
-    //         yellow.removeEventListener("click", pushButton); // disable click yellow
-    //     }
-    // });
-
-    // function pushButton(e) {
-    //     let btnKey = "";
-    //     if (e instanceof KeyboardEvent) {
-    //         // keyboard event (typing 'R', 'G', 'B', 'Y')
-    //         btnKey = e.keyCode;
-    //     } else if (e instanceof MouseEvent) {
-    //         // click event per button
-    //         btnKey = this.dataset.key;
-    //     }
-    //     // get dataset.key from audio element
-    //     const audio = document.querySelector(`audio[data-key="${btnKey}"]`);
-    //     // get dataset.key from div.btn element
-    //     const btn = document.querySelector(`.btn[data-key="${btnKey}"]`);
-    //     if (!audio) return;
-    //     // reset audio to 0
-    //     audio.currentTime = 0;
-    //     stopSounds();
-    //     // play current audio element
-    //     audio.play();
-    //     // if (turn <= 5) {
-    //     //     setTimeout(() => {
-    //     //         audio.pause();
-    //     //         audio.currentTime = 0;
-    //     //     }, 420);
-    //     // } else if (turn <= 13 && turn >= 6) {
-    //     //     setTimeout(() => {
-    //     //         audio.pause();
-    //     //         audio.currentTime = 0;
-    //     //     }, 320);
-    //     // } else if (turn > 13) {
-    //     //     setTimeout(() => {
-    //     //         audio.pause();
-    //     //         audio.currentTime = 0;
-    //     //     }, 220);
-    //     // }
-    //     // add 'active' class
-    //     btn.classList.add("active");
-    //     // timeout to remove 'active' class
-    //     setTimeout(() => {
-    //         btn.classList.remove("active");
-    //     }, 100);
-    // }
+    function pushButton(e) {
+        if (on) {
+            let btnKey = "";
+            if (e instanceof KeyboardEvent) {
+                // keyboard event (typing 'R', 'G', 'B', 'Y')
+                btnKey = e.keyCode;
+            } else if (e instanceof MouseEvent) {
+                // click event per button (parseInt to match KeyboardEvents)
+                btnKey = parseInt(this.dataset.key);
+            }
+            switch (btnKey) {
+                case 71:
+                    playerOrder.push(1);
+                    break;
+                case 82:
+                    playerOrder.push(2);
+                    break;
+                case 89:
+                    playerOrder.push(3);
+                    break;
+                case 66:
+                    playerOrder.push(4);
+                    break;
+            }
+            check();
+            // get dataset.key from audio element
+            const audio = document.querySelector(`audio[data-key="${btnKey}"]`);
+            // get dataset.key from div.btn element
+            const btn = document.querySelector(`.btn[data-key="${btnKey}"]`);
+            if (!audio) return;
+            // reset audio to 0
+            audio.currentTime = 0;
+            stopSounds();
+            // play current audio element
+            audio.play();
+            // add 'active' class
+            btn.classList.add("active");
+            // timeout to remove 'active' class
+            if (!win) {
+                setTimeout(() => {
+                    btn.classList.remove("active");
+                }, 100);
+            }
+        }
+    }
 
 });
