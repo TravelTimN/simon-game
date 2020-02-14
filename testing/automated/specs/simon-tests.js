879f28d73e2659d16e8c179a3d2e35b9a609990d
Tim Nelson <traveltimn@gmail.com>
2020-02-14 21:57:14 +0000

    Add more Jasmine automated tests

--- testing/automated/specs/simon-tests.js
+++ testing/automated/specs/simon-tests.js
@@ -40,20 +40,25 @@ describe("Simon Memory Game Testing", () => {
         it("should exist", () => {
             expect($("powerOn")).toBeDefined();
         });
-        it("should be off by default (power=off)", () => {
+        it("should be 'off' by default", () => {
             let isOn = false;
             expect(isOn).not.toBeTruthy();
         });
-        it("should not be Simon's turn (power=off)", () => {
+        it("should not be Simon's turn (if power=off)", () => {
             let simonTurn = false;
             expect(simonTurn).not.toBeTruthy();
         });
-        it("should call the disablePlayer() function (power=off)", () => {
+        it("should trigger the strictButton to be false (during power off)", () => {
+            let isStrict = false;
+            expect($("#powerButton")).toBeChecked();
+            expect(isStrict).not.toBeTruthy();
+        });
+        it("should call the disablePlayer() function (during power off)", () => {
             spyOn(window, "disablePlayer");
             disablePlayer();
             expect(window.disablePlayer).toHaveBeenCalled();
         });
-        it("should call the disableStart() function after 500ms (power=off)", () => {
+        it("should call the disableStart() function after 500ms (during power off)", () => {
             jasmine.clock().install();
             jasmine.clock().tick(500);
             spyOn(window, "disableStart");
@@ -61,7 +66,7 @@ describe("Simon Memory Game Testing", () => {
             expect(window.disableStart).toHaveBeenCalled();
             jasmine.clock().uninstall();
         });
-        it("should call the disableStrict() function after 500ms (power=off)", () => {
+        it("should call the disableStrict() function after 500ms (during power off)", () => {
             jasmine.clock().install();
             jasmine.clock().tick(500);
             spyOn(window, "disableStrict");
@@ -69,7 +74,7 @@ describe("Simon Memory Game Testing", () => {
             expect(window.disableStrict).toHaveBeenCalled();
             jasmine.clock().uninstall();
         });
-        it("should call the disableColors() function after 500ms (power=off)", () => {
+        it("should call the disableColors() function after 500ms (during power off)", () => {
             jasmine.clock().install();
             jasmine.clock().tick(500);
             spyOn(window, "disableColors");
@@ -77,38 +82,49 @@ describe("Simon Memory Game Testing", () => {
             expect(window.disableColors).toHaveBeenCalled();
             jasmine.clock().uninstall();
         });
-        // it("should clear the gameSpeed interval after 500ms (power=off)", () => {
-        //     timerCallback = jasmine.createSpyObj("setInterval");
-        //     jasmine.clock().install();
-        //     setTimeout(() => {
-        //         timerCallback();
-        //     }, 500);
-        //     expect(setInterval).not.toHaveBeenCalled();
-        //     jasmine.clock().tick(501);
-        //     expect(setInterval).toHaveBeenCalled();
-        //     jasmine.clock().uninstall();
-        // });
-        it("should trigger the game console to turn on", () => {
+        it("should clear the gameSpeed interval after 500ms (during power off)", () => {
+            intervalCallback = jasmine.createSpy("clearInterval", gameSpeed);
+            jasmine.clock().install();
+            setTimeout(() => {
+                intervalCallback();
+            }, 500);
+            expect(intervalCallback).not.toHaveBeenCalled();
+            jasmine.clock().tick(501);
+            expect(intervalCallback).toHaveBeenCalled();
+            jasmine.clock().uninstall();
+        });
+        it("should clear the playerTimer timeout after 500ms (during power off)", () => {
+            timeoutCallback = jasmine.createSpy("clearTimeout", playerTimer);
+            jasmine.clock().install();
+            setTimeout(() => {
+                timeoutCallback();
+            }, 500);
+            expect(timeoutCallback).not.toHaveBeenCalled();
+            jasmine.clock().tick(501);
+            expect(timeoutCallback).toHaveBeenCalled();
+            jasmine.clock().uninstall();
+        });
+        it("should trigger the game console to turn on if clicked", () => {
             let isOn = true;
             expect($("#powerButton")).toBeChecked();
             expect(isOn).toBeTruthy();
         });
-        it("should call the enableStart() function (power=on)", () => {
+        it("should call the enableStart() function (during power on)", () => {
             spyOn(window, "enableStart");
             enableStart();
             expect(window.enableStart).toHaveBeenCalled();
         });
-        it("should call the enableStrict() function (power=on)", () => {
+        it("should call the enableStrict() function (during power on)", () => {
             spyOn(window, "enableStrict");
             enableStrict();
             expect(window.enableStrict).toHaveBeenCalled();
         });
-        it("should call the inactiveGame() function (power=on)", () => {
+        it("should call the inactiveGame() function (during power on)", () => {
             spyOn(window, "inactiveGame");
             inactiveGame();
             expect(window.inactiveGame).toHaveBeenCalled();
         });
-        it("should turn the Level Counter 'on' (power=on)", () => {
+        it("should turn the Level Counter 'on' (during power on)", () => {
             $("#levelCounter").addClass("on").text("ON");
             expect($("#levelCounter")).toHaveClass("on");
             expect($("#levelCounter")).toHaveText("ON");
@@ -120,20 +136,20 @@ describe("Simon Memory Game Testing", () => {
         it("should exist", () => {
             expect($("#startButton")).toBeDefined();
         });
-        it("should be 'disabled' (power=off)", () => {
+        it("should be 'disabled' (if power=off)", () => {
             $("#startButton").prop("disabled", true);
             expect($("#startButton")).toHaveAttr("disabled");
         });
-        it("should have a 'default' cursor (power=off)", () => {
+        it("should have a 'default' cursor (if power=off)", () => {
             expect($(".startButton label")).toHaveCss({cursor: "default"});
         });
         beforeEach(() => {
             $("#powerButton").click();
         });
-        it("should be 'enabled' (power=on)", () => {
+        it("should be 'enabled' (if power=on)", () => {
             expect($("#startButton")).not.toHaveAttr("disabled");
         });
-        it("should have a 'pointer' cursor (power=on)", () => {
+        it("should have a 'pointer' cursor (if power=on)", () => {
             $(".startButton label").css("cursor", "pointer");
             expect($(".startButton label")).toHaveCss({cursor: "pointer"});
         });
@@ -144,20 +160,20 @@ describe("Simon Memory Game Testing", () => {
         it("should exist", () => {
             expect($("#strictButton")).toBeDefined();
         });
-        it("should be 'disabled' (power=off)", () => {
+        it("should be 'disabled' (if power=off)", () => {
             $("#strictButton").prop("disabled", true);
             expect($("#strictButton")).toHaveAttr("disabled");
         });
-        it("should have a 'default' cursor (power=off)", () => {
+        it("should have a 'default' cursor (if power=off)", () => {
             expect($(".strictButton label")).toHaveCss({cursor: "default"});
         });
         beforeEach(() => {
             $("#powerButton").click();
         });
-        it("should be 'enabled' (power=on)", () => {
+        it("should be 'enabled' (if power=on)", () => {
             expect($("#strictButton")).not.toHaveAttr("disabled");
         });
-        it("should have a 'pointer' cursor (power=on)", () => {
+        it("should have a 'pointer' cursor (if power=on)", () => {
             $(".strictButton label").css("cursor", "pointer");
             expect($(".strictButton label")).toHaveCss({cursor: "pointer"});
         });
@@ -200,3 +216,26 @@ describe("Simon Memory Game Testing", () => {
 
 
 });
+
+
+/*
+    * CREDITS
+    * Helpful Links for Jasmine testing
+*/
+
+/* Jasmine cheatsheet */
+// https://devhints.io/jasmine
+
+/* setFixtures */
+// https://lostechies.com/derickbailey/2011/10/14/quick-hack-to-work-around-jasmine-jquery-fixture-limitations/
+
+/* createSpy + createSpyObj */
+// https://scriptverse.academy/tutorials/jasmine-createspy-createspyobj.html
+
+/* clearTimeout + setTimeout */
+// https://stackoverflow.com/a/50883535
+// https://makandracards.com/makandra/32477-testing-settimeout-and-setinterval-with-jasmine
+
+/* addEventListener */
+// https://stackoverflow.com/a/32576013
+// even better: use jQuery $("#el").click();
